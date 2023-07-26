ca613b6529ffdcb72f7e2d6055da5231861a56f1
p1peCode <valeryroj@yandex.ru>
2023-07-26 18:21:59 +0300

    first commit

---
+++ src/main/java/com/example/mavito/MavitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavito/MavitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
